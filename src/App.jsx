1107d818f68c394faeb309ad6d1ada0c970af48f
FLeiras <fleiras18@gmail.com>
2026-06-04 14:58:10 -0300

    fix: update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import { Footer } from "./components/Footer/Footer";
 function App() {
   return (
     <div className="site-shell">
-      <Navbar />
+      <NavBar />
       <main>
         <Hero />
         <Pillars />
